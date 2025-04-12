dfecd025a181d1d323d683304502cc922453caec
Rodrigo <rodrigo.argondizo@ufu.br>
2025-04-12 08:42:58 -0300

    Refinamento dos comentarios do código

---
+++ Main_Project/build/CMakeFiles/dnp3_modbus_integration.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dnp3_modbus_integration.
+# This may be replaced when dependencies are built.
---
+++ Main_Project/build/CMakeFiles/dnp3_modbus_integration.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for dnp3_modbus_integration.
